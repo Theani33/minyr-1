9fb0da06fd66c780b273bbe8fe615129a5aa7a35
Theani33 <Theani@uia.no>
2023-05-05 01:06:04 +0200

    123

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -1,47 +1,71 @@
 package yr
 
 import (
+	"fmt"
+	"github.com/Theani33/funtemps/conv"
+	"os"
 	"testing"
 )
 
-func TestCelsiusToFahrenheitString(t *testing.T) {
-     type test struct {
-	input string
-	want string
-     }
-     tests := []test{
-	     {input: "6", want: "42.8"},
-	     {input: "0", want: "32.0"},
-     }
+func TestLineCount(t *testing.T) {
 
-     for _, tc := range tests {
-	     got, _ := CelsiusToFahrenheitString(tc.input)
-	     if !(tc.want == got) {
-		     t.Errorf("expected %s, got: %s", tc.want, got)
-	     }
-     }
+	//åpner filen som skal leses
+	file, err := os.Open("kjevik-temp-fahr-20220318-20230318.csv")
+	if err != nil {
+		t.Fatalf("failed to open file %v", err)
+	}
+	defer file.Close()
+
+	//teller antall linjer i filen
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	//sjekker at linjeantallet samsvarer med det vi vil ha
+	expectedCount := 16756
+	if lineCount != expectedCount {
+		t.Errorf("Unexpected number of lines. Got %v, expected %v", lineCount, expectedCount)
+	}
+
+}
+func TestCelsiusToFahrenheitString(t *testing.T) {
+	type test struct {
+		input string
+		want  string
+	}
+	tests := []test{
+		{input: "6", want: "42.8"},
+		{input: "0", want: "32.0"},
+	}
+
+	for _, tc := range tests {
+		got, _ := CelsiusToFahrenheitString(tc.input)
+		if !(tc.want == got) {
+			t.Errorf("expected %s, got: %s", tc.want, got)
+		}
+	}
 }
 
-// Forutsetter at vi kjenner strukturen i filen og denne implementasjon 
+// Forutsetter at vi kjenner strukturen i filen og denne implementasjon
 // er kun for filer som inneholder linjer hvor det fjerde element
 // på linjen er verdien for temperatrmaaling i grader celsius
 func TestCelsiusToFahrenheitLine(t *testing.T) {
-     type test struct {
-	input string
-	want string
-     }
-     tests := []test{
-	     {input: "Kjevik;SN39040;18.03.2022 01:50;6", want: "Kjevik;SN39040;18.03.2022 01:50;42.8"},
-	     //{input: "Kjevik;SN39040;18.03.2022 01:50", want: ""},
+	type test struct {
+		input string
+		want  string
+	}
+	tests := []test{
+		{input: "Kjevik;SN39040;18.03.2022 01:50;6", want: "Kjevik;SN39040;18.03.2022 01:50;42.8"},
+		//{input: "Kjevik;SN39040;18.03.2022 01:50", want: ""},
 
-     }
+	}
 
-     for _, tc := range tests {
-	     got, _ := CelsiusToFahrenheitLine(tc.input)
-	     if !(tc.want == got) {
-		     t.Errorf("expected %s, got: %s", tc.want, got)
-	     }
-     }
-
-	
+	for _, tc := range tests {
+		got, _ := CelsiusToFahrenheitLine(tc.input)
+		if !(tc.want == got) {
+			t.Errorf("expected %s, got: %s", tc.want, got)
+		}
+	}
 }
